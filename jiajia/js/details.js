3ff7e72d00206da42e573794c044f4bae2feeb0a
1931967674 <1931967674@qq.com>
2019-11-12 23:40:18 +0800

    放大镜功能结束

--- jiajia/js/details.js
+++ jiajia/js/details.js
@@ -29,55 +29,55 @@ $(".glass").hover(function () {
           })
 })
 // 放大镜的功能
- var list = document.querySelector(".list"),
-     imgs = list.querySelectorAll("img"),
-     img = document.querySelector(".pic img"),
-     
-     pic = document.querySelector(".itemarea .pic"),
-     cover = document.querySelector(".cover"),
-    detail = document.querySelector('.detail');
-    //  console.log(img);
- list.addEventListener("mousemove", function (e) {
-     if (e.target.tagName == "IMG") { //判断ul里面的li的img是否等于IMG，如果等于，执行，这是js的事件委托，一种机制，父元素设置，给子元素触发
-         img.src = e.target.src; //将当前元素img的src赋给img
-         detail.style.backgroundImage = 'url(' + e.target.src + ')';
-         imgs.forEach(function (item) { //forEach() 方法用于调用数组的每个元素，并将元素传递给回调函数
-             item.className = ""; //让img的类名等于空，循环判断，清空所有img的类名
-         })
-         e.target.className = "current"; //给当前元素设置类名
-     }
- })
- pic.addEventListener("mousemove", function (e) {
-     var x = e.clientX,
-         y = e.clientY,
-         cx = pic.getBoundingClientRect().left,
-         cy = pic.getBoundingClientRect().top,
-         tx = x - cx - 75,
-         ty = y - cy - 75;
-     if (tx < 0) {
-         tx = 0;
-     }
-     if (ty < 0) {
-         ty = 0;
-     }
-     if (tx > 250) {
-         tx = 250;
-     }
-     if (ty > 150) {
-         ty =150;
-     }
-     detail.style.backgroundPosition = tx / 250 * 100 + '%' + ty / 250 * 100 + '%';
-     cover.style.left = tx + 'px';
-     cover.style.top = ty + 'px';
- })
-//  --------------------------------
+  var list = document.querySelector(".list"),
+      imgs = list.querySelectorAll("img"),
+      img = document.querySelector(".pic img"),
+      pic = document.querySelector(".itemarea .pic"),
+      cover = document.querySelector(".cover"),
+      detail = document.querySelector('.detail');
+  list.addEventListener("mousemove", function (e) {
+      if (e.target.tagName == "IMG") { //判断ul里面的li的img是否等于IMG，如果等于，执行，这是js的事件委托，一种机制，父元素设置，给子元素触发
+          img.src = e.target.src; //将当前元素img的src赋给img
+          detail.style.backgroundImage = 'url(' + e.target.src + ')';
+          imgs.forEach(function (item) { //forEach() 方法用于调用数组的每个元素，并将元素传递给回调函数
+              item.className = ""; //让img的类名等于空，循环判断，清空所有img的类名
+          })
+          e.target.className = "current"; //给当前元素设置类名
+      }
+  })
+  pic.addEventListener("mousemove", function (e) {
+      var x = e.clientX,
+          y = e.clientY,
+          cx = pic.getBoundingClientRect().left,
+          cy = pic.getBoundingClientRect().top,
+          tx = x - cx - 75,
+          ty = y - cy - 75;
+      if (tx < 0) {
+          tx = 0;
+      }
+      if (ty < 0) {
+          ty = 0;
+      }
+      if (tx > 250) {
+          tx = 250;
+      }
+      if (ty > 150) {
+          ty = 150;
+      }
+      detail.style.backgroundPosition = tx / 250 * 100 + '%' + ty / 150 * 100 + '%';
+      cover.style.left = tx + 'px';
+      cover.style.top = ty + 'px';
+  })
+//  放大镜换图片功能
+var obj = JSON.parse(localStorage.getItem('index')) || "";
+// console.log($(".itemarea .pic img").attr() );
+$(".itemarea .pic img")[0].src = obj.url;
+$(".itemarea .detail").css({
+    background: "url(" + obj.url + ")",
+})
+$(".itemarea .detail")[0].backgroundRepeat = 'no-repeat';
+// 购物车功能 --------------------------------
   
-  function data() {
-
-      this.img = JSON.parse(localStorage.getItem('index')) || "";
-      
-  }
-  data();
 class data{
     constructor() {
         this.obj1 = JSON.parse(localStorage.getItem('wei')) || "";
@@ -121,7 +121,7 @@ class data{
                 }
             }
              
-              console.log(str);
+            //   console.log(str);
             localStorage.setItem("wei", JSON.stringify(str));
         })
     }
