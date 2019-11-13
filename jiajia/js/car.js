d65f0a83b3e15bc56a091a369db49e6e32f789c0
1931967674 <1931967674@qq.com>
2019-11-13 21:03:45 +0800

    购物车求和功能

--- jiajia/js/car.js
+++ jiajia/js/car.js
@@ -7,16 +7,56 @@ class car{
         this.style();
         // console.log(this.res);
         // 绑定点击事件
-         this.click();
+        this.click();
+       
     }
-    // 绑定点击事件
+    
     click() {
+        var sam = 0;
         var that = this;
-
+        // 绑定点击删除事件
         $("tbody")[0].addEventListener("click", trust("dele", function () {
             that.id = this.parentNode.parentNode.getAttribute("index");
             that.del(this);
         }))
+        // 点击求和事件
+        $("tbody")[0].addEventListener("click", trust("election", function () {
+            that.str = [];
+            for (var i = 0; i < $(".election").length; i++){
+                if ($(".election")[i].checked) {
+                     that.num = parseInt($(".election")[i].parentNode.parentNode.lastElementChild.innerHTML);
+                     that.sum();
+                }
+            }
+        that.sqm();
+            
+        }))
+    //     $(".cheA")[0].addEventListener("click", function () {
+    //         for (var i = 0; i < $(".election").length; i++){
+    //             $(".election")[i].checked = "checked";
+    //         }
+            
+    //     })
+    //     $(".che")[0].addEventListener("click", function () {
+    //         for (var i = 0; i < $(".election").length; i++) {
+    //             $(".election")[i].checked = "";
+    //         }
+    //     })
+    }
+    // 求和
+    sum() {
+        this.str.push(this.num);
+        //   this.str = norepeat(this.str);
+         
+    }
+    // 求和方法
+    sqm() {  
+        console.log(this.str);
+      var ssm = 0;
+        for (var i = 0; i < this.str.length; i++){
+            ssm+=this.str[i]
+        }
+        $(".sun").html(ssm);
     }
     // 点击删除
     del(ele) {
@@ -36,7 +76,7 @@ class car{
                 <td>${this.res[i].num}</td>
                 <td>${this.res[i].parse}</td>
                 <td><span class="dele">&times;<span/></td>
-                <td>${parseInt(this.res[i].num)*parseInt(this.res[i].parse)}</td>
+                <td class="sum">${parseInt(this.res[i].num)*parseInt(this.res[i].parse)}</td>
             </tr>`
         }
         $("tbody").html(str);
@@ -53,4 +93,19 @@ function trust(cla, cb) {
             cb.bind(target)();
         }
     }
+}
+// ---------------------------------------------
+// 数组去重的方法
+function norepeat(arr) {
+    arr.sort();
+    //先排序让大概相同的在一个位置，这里为什么说是大概相同 是因为sort排序是把元素当字符串排序的 它和可能排成 1 1 10 11 2 20 3 ... 不是我们想要的从小到大
+    for (var i = 0; i < arr.length - 1; i++) {
+        //还是两两比较 一样删除后面的
+        if (arr[i] == arr[i + 1]) {
+            arr.splice(i, 1);
+            //i-- 和j--同理
+            i--;
+        }
+    }
+    return arr;
 }
